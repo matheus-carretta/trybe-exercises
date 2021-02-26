278c73122074de9d21b044753332808cccff9163
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-02-25 22:52:01 -0300

    Exercicio 9 - Divide todos os numeros por 2 e exibe]

---
+++ bloco_4/dia_2/9-divisao-de-numeros.js
@@ -0,0 +1,12 @@
+let array = [];
+
+for(let index = 1; index <= 25; index += 1){
+  array.push(index);
+}
+
+for(index = 0; index < array.length; index += 1){
+  let division = array[index] / 2;
+  console.log(array[index] + "/ 2 = " + division);
+}
+
+
