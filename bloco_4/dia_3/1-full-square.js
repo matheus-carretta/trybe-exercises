3937fd8bd3edf594e6fc8866f4baa74b73fefb32
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-02-26 18:19:41 -0300

    Atualiza o nome do arquivo do exercicio 1

---
+++ bloco_4/dia_3/1-full-square.js
@@ -0,0 +1,16 @@
+// 1 - Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+let squareSize = 5;
+let square = '';
+
+if(squareSize <= 1){
+  console.log("Não tem como fazer um quadrado com 1 asterisco ou menos");
+}else{
+  for(let index = 0; index < squareSize; index += 1){
+    square = square + '*';
+  }
+  
+  for(index = 0; index < squareSize; index += 1){
+    console.log(square);
+  }
+}
