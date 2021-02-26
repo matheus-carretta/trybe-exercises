4dd95903c346dd15a65e7cf235680f96a71bcffb
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-02-25 23:49:46 -0300

    Bonus 1 - Ordenda os valores do Array em ordem Cresecnte

--- bloco_4/dia_2/5-maior-numero.js
+++ bloco_4/dia_2/5-maior-numero.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let biggestNumber = numbers[0]
+let biggestNumber = numbers[0];
 
 for(let index = 1; index < numbers.length; index +=1){
   if(numbers[index] > biggestNumber){
---
+++ bloco_4/dia_2/bubllesort.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1){
+  for (let jindex = 0; jindex < numbers.length - index; jindex += 1){
+    if(numbers[jindex] > numbers[jindex+1]){
+      let aux = numbers[jindex];
+      numbers[jindex] = numbers[jindex+1];
+      numbers[jindex+1] = aux;
+    }
+  }
+}
+
+console.log(numbers);
