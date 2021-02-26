4c88c4481aac380e554d93fdda2f81469bed6f00
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-02-25 21:33:34 -0300

    Exercicio 3 - Calcula media aritmetica

---
+++ bloco_4/dia_2/3-media-aritmetica.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let plus = 0;
+
+for(let index = 0; index < numbers.length; index +=1){
+  plus = plus + numbers[index];
+}
+
+average = plus / numbers.length;
+
+console.log("Soma dos valores: " + plus);
+console.log("Quantidade de valores: "  + numbers.length);
+console.log("Média aritmética é: " + average);
