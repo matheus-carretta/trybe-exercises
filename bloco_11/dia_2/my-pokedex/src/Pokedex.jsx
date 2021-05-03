5ebc80a6b0a045539d090eff099f4af72c477185
Matheus Carretta <matheuscarretta@gmail.com>
2021-05-02 21:57:56 -0300

    Atividade realizada

--- bloco_11/dia_2/my-pokedex/src/Pokedex.jsx
+++ bloco_11/dia_2/my-pokedex/src/Pokedex.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import data from './data';
+import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component{
   render(){
-    return
+    return (
+      <div className='pokemonList'>
+        {data.map((pokemon) => <Pokemon pokemon={pokemon} key={pokemon.id} />)}
+      </div>
+    )
   }
 }
+
+export default Pokedex;
